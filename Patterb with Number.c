bb137424bc7d1f73d87aaf578fd91f97c431f527
Barshon Basak <127104156+barshon-basak@users.noreply.github.com>
2023-05-17 18:30:47 +0600

    Add files via upload

---
+++ Patterb with Number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+int row,col,n;
+
+n=5;
+
+for(row=1;row<=n;row++)
+{
+    for(col=1;col<=row;col++)
+    printf ("%d",col);
+    printf ("\n");
+}
+
+for(row=n-1;row>=1;row--)
+{
+    for(col=1;col<=row;col++)
+    printf ("%d",col);
+    printf ("\n");
+}
+
+
+    return 0;
+}
